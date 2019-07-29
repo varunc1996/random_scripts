fc7667de9dd2497d036923e9b41c50c7d70b7cc0
Varun Cherukuri <varunc1996@gmail.com>
2019-07-29 10:19:08 -0700

    Fix disconnect script

--- tunnelblick_scripts/disconnect_vpn.sh
+++ tunnelblick_scripts/disconnect_vpn.sh
@@ -1,5 +1,5 @@
 # Disconnect all vpns
 osascript disconnect_staging.scpt;
 osascript disconnect_production.scpt;
-osascript disconnect_eu;
+osascript disconnect_eu.scpt;
 osascript disconnect_testpad.scpt;
